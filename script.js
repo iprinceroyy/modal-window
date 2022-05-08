e96adb5a154802c61819c34b96e9964ca6a78780
Prince Roy <princeku.ray143@gmail.com>
2022-05-08 20:51:52 +0530

    handle escape key

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 //for open modal window
-
 const openModal = () => {
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
@@ -23,3 +22,12 @@ const closeModal = () => {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+//Handling escape key
+document.addEventListener('keydown', e => {
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+});
