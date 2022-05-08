95614a9b7e71163319a96b78b907b6aa80319cc7
Prince Roy <princeku.ray143@gmail.com>
2022-05-08 20:18:56 +0530

    close window functionality

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+//for open modal window
 for (let i = 0; i < btnsOpenModal.length; i++) {
     btnsOpenModal[i].addEventListener('click', () => {
         console.log(`Button clicked ${i + 1}`);
@@ -14,7 +15,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 }
 
 //for closing modal window
-btnCloseModal.addEventListener('click', () => {
+const closeModal = () => {
     modal.classList.add('hidden');
     overlay.classList.add('hidden');
-});
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
